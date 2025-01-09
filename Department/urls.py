45f9a30a15e8f81a4f353ea9c5cd2f233d47acbb
sayalichalke <Sayalichalke44gamil.com>
2025-01-09 08:21:27 +0000

    role update

--- Department/urls.py
+++ Department/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('createdepartment',views.createDepartment),
     path('delete/<int:did>',views.Deletedepartment),
     path('edit/<int:did>',views.updateDepartment),
+        #Roles:
+    path('viewrole',views.viewRole),
+    path('addrole',views.addRole),
+    path('deleterole/<int:roleid>',views.Deleterole),
+    path('updaterole/<int:roleid>',views.Updaterole),
 ]
 
--- Department/views.py
+++ Department/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,redirect
-from Department.models import Department
+from django.shortcuts import render,redirect, get_object_or_404
+from Department.models import Department,Roles
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
     data=Department.objects.filter(status=True)
@@ -25,16 +27,89 @@ def Deletedepartment(request, did):
     dept.save()
     return redirect('/')
 
+# def updateDepartment(request, did):
+#     if request.method == "GET":
+#         d = Department.objects.filter(dept_id=did) 
+#         context = {}
+#         context['dept'] = d #in case if we use get
+#         return render(request,'updatedeparment.html',context)
+#     else:
+#         dep = Department.objects.filter(dept_id = did)
+#         # d is the queryset, on Queryset, we can call update and delete
+#         n = request.POST['dept_name']
+#         d = request.POST['description']
+#         dep.update(dept_name=n, description =d)
+#         return redirect('/')
+
 def updateDepartment(request, did):
     if request.method == "GET":
-        d = Department.objects.filter(dept_id=did) 
-        context = {}
-        context['department'] = d #in case if we use get
-        return render(request,'updatedeparment.html',context)
-    else:
-        dep = Department.objects.filter(dept_id = did)
-        # d is the queryset, on Queryset, we can call update and delete
+        # Get a single department object
+        d = get_object_or_404(Department, dept_id=did)
+        context = {"dept": d}
+        return render(request, 'updatedeparment.html', context)
+
+    elif request.method == "POST":
+        # Retrieve the object to update
+        dep = Department.objects.filter(dept_id=did)
+
+        # Fetch form data
         n = request.POST['dept_name']
         d = request.POST['description']
-        dep.update(dept_name=n, description =d)
+
+        # Update the department fields
+        dep.update(dept_name=n, description=d)
+
+        # Redirect to homepage or department list
         return redirect('/')
+    
+
+# View roles (only active)
+def viewRole(request):
+    data = Roles.objects.filter(status=True)
+    context = {'roles': data}
+    return render(request, 'viewrole.html', context)
+
+# Add a role (only for admins)
+def addRole(request):
+    # user = Users.objects.get(username=request.user.username) if request.user.is_authenticated else None
+    # if not user or not is_admin(user):
+    #     messages.error(request, 'You do not have permission to perform this action.')
+    #     return redirect('/')
+    
+    if request.method == 'GET':
+        return render(request, 'addrole.html')
+    else:
+        n = request.POST['role_name']
+        d = request.POST['description']
+        Roles.objects.create(role_name=n, description=d)
+        return redirect('/viewrole')
+
+# Delete role (only for admins)
+def Deleterole(request, roleid):
+    # user = Users.objects.get(username=request.user.username) if request.user.is_authenticated else None
+    # if not user or not is_admin(user):
+    #     messages.error(request, 'You do not have permission to perform this action.')
+    #     return redirect('/')
+    
+    role = Roles.objects.get(role_id=roleid)
+    role.status = False
+    role.save()
+    return redirect('/viewrole')
+
+# Update role (only for admins)
+def Updaterole(request, roleid):
+    # user = Users.objects.get(username=request.user.username) if request.user.is_authenticated else None
+    # if not user or not is_admin(user):
+    #     messages.error(request, 'You do not have permission to perform this action.')
+    #     return redirect('/')
+    
+    r = Roles.objects.get(role_id=roleid)
+    if request.method == 'GET':
+        context = {'role': r}
+        return render(request, 'updaterole.html', context)
+    else:
+        n = request.POST['role_name']
+        r.description = request.POST['description']
+        r.role_name = n
+        r.save()
+        return redirect('/viewrole')
